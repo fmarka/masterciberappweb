d408e52769821126d4a9d5da007cf1d74c58a655
fmarka <136133465+fmarka@users.noreply.github.com>
2023-07-07 14:37:23 -0500

    api.py

--- api.py
+++ api.py
@@ -1,21 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from query import get_company_information
-from query_walapop import get_walapop
+from query_wallapop import get_wallapop
+import json
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return ' Hello World!'\
-
+    return 'Hello World!'
 @app.route('/api/yahoo')
-def get_company():
-    symbol = "DIS"
-    return get_company_information(symbol)
 
-@app.route('/api/walapop')
+def get_company():
+    symbol = request.args.get('ticker')
+    if symbol is None:
+        return Response(json.dumps({"error": "missing 'ticker' query-parameter"}), status=400, mimetype='application/json')
+    return Response(json.dumps(get_company_information(symbol)), status=200, mimetype='application/json')
+@app.route('/api/wallapop')
 def get_product_and_price_average():
     query = request.args.get('search_text')
-    return get_walapop(query)
-
+    if query is None:
+        return Response(json.dumps({"error": "missing 'search_text' query-parameter"}), status=400, mimetype='application/json')
+    return Response(json.dumps(get_wallapop(query)), status=200, mimetype='application/json')
 if __name__ == '__main__':
     app.run()
